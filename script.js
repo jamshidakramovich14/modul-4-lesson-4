409ce5593a17aa05eeb199d4bf1c725e08ce2c9a
jamshidakramovich14 <jamshidturdiyev103>
2024-02-16 08:02:48 +0500

    modul 4 lesson 4

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// 1-masala
+// let a = prompt("sonni kiriting");
+// let b = prompt("sonni kiriting");
+// console.log(a - b);
+
+// 2-masala
+// let a = prompt("sonni kiriting");
+// console.log(a * a);
+// console.log(a * a * a);
+// console.log(a * a * a *a);
+
+// 8-masala
+// let n = prompt("sonni kiriting");
+// console.log(n + " * 2 = " + n * 2);
+// console.log(n + " * 3 = " + n * 3);
+// console.log(n + " * 4 = " + n * 4);
+// console.log(n + " * 5 = " + n * 5);
+// console.log(n + " * 6 = " + n * 6);
+// console.log(n + " * 7 = " + n * 7);
+// console.log(n + " * 8 = " + n * 8);
+// console.log(n + " * 9 = " + n * 9);
+// console.log(n + " * 10 = " + n * 10);
